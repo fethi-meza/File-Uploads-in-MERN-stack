c6ee34e5d0c61983f7a01559ac0a08b80726ae90
fethi <fethi.meziane.fm@gmail.com>
2024-06-13 11:44:19 +0000

    n

--- server/constants/Multer.js
+++ server/constants/Multer.js
@@ -3,27 +3,15 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../public/images'); 
+        cb(null, '../public/images'); // Adjust the destination path as per your project structure
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         const ext = path.extname(file.originalname);
-        cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+        cb(null, 'image-' + uniqueSuffix + ext);
     }
 });
 
-const fileFilter = (req, file, cb) => {
-    // Accept only image files
-    if (file.mimetype.startsWith('image')) {
-        cb(null, true);
-    } else {
-        cb(new Error('File type not supported'), false);
-    }
-};
-
-const upload = multer({ 
-    storage: storage,
-    fileFilter: fileFilter
-}).single('image'); 
+const upload = multer({ storage: storage }).single('image'); // Ensure 'image' matches the field name in your form
 
 module.exports = upload;
